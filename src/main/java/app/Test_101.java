20a388c3aabcc750f20fee1aa646da79bb4040d6
Gin <shuaka60@gmail.com>
2022-01-09 10:32:20 +0800

    test

--- src/main/java/app/Test_101.java
+++ src/main/java/app/Test_101.java
@@ -5,7 +5,7 @@ public class Test_101 {
 	public static void main(String[] args) {
 		System.out.println("自動更新實在是神奇了!");
 		System.out.println("!");
-		
+		System.out.println("************");
 		
 
 	}
